b2b0cdf596acca6062dfc617bb6b1a2cf5d77a60
Votre Nom <joelgantshua@gmail.com>
2025-09-25 16:28:15 +0100

    footer

--- src/components/AboutMeprecc.jsx
+++ src/components/AboutMeprecc.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/aboutMeprecc.css";
-import mepreccImage from "../assets/images/meprecc.webp";
+import mepreccImage from "../assets/images/meprec.webp";
 
 const AboutMeprecc = () => {
   return (
