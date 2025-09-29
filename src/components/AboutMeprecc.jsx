a418c992b3b91a4cde6ed5cf6322e1a2f108564f
Votre Nom <joelgantshua@gmail.com>
2025-09-29 10:28:20 +0100

    modofication rdsmeprec

--- src/components/AboutMeprecc.jsx
+++ src/components/AboutMeprecc.jsx
@@ -10,16 +10,8 @@ const AboutMeprecc = () => {
       </div>
       <div className="about-text">
         <h2>À propos de la Meprecc SA</h2>
-        <p>
-          La Meprecc SA est un ministère dynamique dédié à l'édification spirituelle,
-          à la formation et à l'accompagnement des croyants. Dirigée par la prophétesse
-          Raissa Mutamba, elle œuvre pour inspirer et transformer les vies à travers
-          la foi, l'art et l'enseignement.
-        </p>
-        <p>
-          Ses actions couvrent divers domaines : enseignements bibliques,
-          projets artistiques, événements communautaires et initiatives sociales
-          qui renforcent la mission spirituelle et culturelle.
+        <p>Fondée le 19 avril 2013, la Meprecc SA est une Église dynamique, dédiée au réveil, à la formation et à l’accompagnement des croyants. 
+          Sous la direction de la prophétesse Raïssa Mutamba, l’Église Meprecc SA œuvre à inspirer et transformer des vies à travers la foi, la puissance et l’excellence.
         </p>
       </div>
     </section>
--- src/components/AboutRds.jsx
+++ src/components/AboutRds.jsx
@@ -7,21 +7,26 @@ const AboutRds = () => {
     <>
       {/* Ligne de séparation entre Meprecc et RDS */}
       <div className="section-divider"></div>
-
       <section id="about-rds" className="about-rds">
-        <div className="about-text">
-          <h2>À propos du RDS</h2>
-          <p>
-            RDS Africa est une plateforme en ligne initiée par Raissa Mutamba
-            pour connecter et former les créatifs chrétiens. Elle offre des
-            ressources pour la musique, les arts et la croissance spirituelle,
-            visant à inspirer et guider les talents émergents.
-          </p>
-        </div>
-        <div className="about-image">
-          <img src={rdsImage} alt="RDS Africa" />
-        </div>
-      </section>
+  <div className="about-text">
+    <h2>À propos du RDS</h2>
+    <p>
+      Existant depuis 2016, le RDS (Réseau Du Saint Esprit) est une
+      infrastructure spirituelle conduite par l’Esprit, pensée pour servir,
+      mobiliser et nourrir spirituellement les croyants à travers trois axes
+      principaux d’expression :
+    </p>
+    <ol>
+      <li>Formations et retraites en ligne</li>
+      <li>Centre de prière en ligne</li>
+      <li>Branche d’actions et d’outreach</li>
+    </ol>
+  </div>
+  <div className="about-image">
+    <img src={rdsImage} alt="RDS Africa" />
+  </div>
+</section>
+
     </>
   );
 };
